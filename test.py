feee08c07562083b7befd5db4650f018de6f6229
Muhammad Hashir Awaiz <hashir.avaiz@gmail.com>
2025-08-07 16:43:58 +0500

    Revert "Initial RAG Chatbot"
    This reverts commit 5695482c75c57b51bcb161dd0b67bf82713aa619.

--- test.py
+++
@@ -1,7 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage
-
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", temperature=0)
-
-response = llm.invoke([HumanMessage(content="Who are you?")])
-print(response.content)
